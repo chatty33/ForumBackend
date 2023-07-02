ce12361e79bdcf50a16f280903b4d3bf95c4b657
Chaitanya Chopra <56465959+chatty33@users.noreply.github.com>
2023-07-03 00:29:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes)
+app.use("/user", userRouter);
+
+const CONNECTION_URL = "mongodb+srv://dev-ritvik:bossman123@cluster0.8vhpcps.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5100;
+
+mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
+    
+    
